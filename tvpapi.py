b505205bcf5eb7619f159da39027b7037236721b
Patrick Kubiak <patrick@antuple.net>
2018-06-07 01:11:24 -0400

    Episde naming title format: Series - S01E001

--- tvpapi.py
+++ tvpapi.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import string
+import os
 
 class TVPApi:
   def __init__(self, url, dest="downloads/", api_listing="http://www.api.v3.tvp.pl/shared/listing.php?dump=json&count&parent_id=", api_tokenizer="http://www.tvp.pl/shared/cdn/tokenizer_v2.php?object_id="):
@@ -12,6 +13,10 @@ class TVPApi:
     self.dest = dest
     self.api_listing = api_listing
     self.api_tokenizer = api_tokenizer
+
+    # check if destination directory exists
+    if not os.path.exists(self.dest):
+      os.makedirs(self.dest)
 
   def get_series_id(self):
     url_data = requests.get(self.url).text
@@ -43,7 +48,7 @@ class TVPApi:
     endpoint_resp = requests.get(self.api_listing + self.series_episodes_id)
     endpoint_json = endpoint_resp.json()
 
-    # store episode ids
+    # store season ids
     self.episodes = dict()
     for item in endpoint_json["items"]:
       self.episodes[item["title"]] = {'season_id': str(item["asset_id"])}
@@ -55,21 +60,30 @@ class TVPApi:
       self.episodes[season]["episode_ids"] = list()
 
       for item in endpoint_json["items"]:
-        # title format: Series - E001
-        if "website_title" in item and "original_title" in item:
-          title = item["website_title"]
-          episode_num = item["original_title"]
-        elif "website_title" in item and "web_name" in item:
-          title = item["website_title"]
-          episode_re = re.search("odc(-*)(\d*)", item["web_name"])
-          episode_num = episode_re.group(2)
+        # title format: Series - S01E001
+        if "website_title" in item:
+          title = string.capwords(item["website_title"])
+          if "original_title" in item:
+            episode_num = item["original_title"]
+          elif "web_name" in item:
+            episode_num = item["web_name"]
         else:
           print(item)
           raise Exception('Cannot find episode title.')
 
-        title = string.capwords(title) + " - E" + episode_num.zfill(3)
-        #print(title)
+        episode_num = re.search("([^\d]*)(\d*)", episode_num).group(2)
+        season_num = season.strip()
 
+        season_roman = re.search("[IVXLCDM]+", season_num)
+        if season_roman is not None:
+          # season is a roman numeral, convert roman numerals
+          season_num = str(self.roman_to_int(season_roman.group(0)))
+
+        # season filter only number
+        season_num = re.search("\d+", season_num).group(0)
+
+        # store episode id and title
+        title = string.capwords(title) + " - S" + season_num.zfill(2) + "E" + episode_num.zfill(3)
         episode = {'asset_id': item["asset_id"], 'title': title}
         self.episodes[season]["episode_ids"].append(episode)
 
@@ -79,7 +93,6 @@ class TVPApi:
     self.get_episodes_id()
     self.get_season_ids()
     self.get_season_episode_ids()
-    print()
 
     # sort
     return sorted(self.episodes.items())
@@ -107,7 +120,7 @@ class TVPApi:
     print("Writing " + output_path)
     r = requests.get(url, stream=True)
     with open(output_path, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024): 
+        for chunk in r.iter_content(100 * 2 ** 20): # 100MB chunk size
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
 
@@ -117,12 +130,22 @@ class TVPApi:
     except KeyError:
         return 0
 
+  def roman_to_int(self, s):
+    rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    int_val = 0
+    for i in range(len(s)):
+      if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
+         int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+      else:
+         int_val += rom_val[s[i]]
+    return int_val
+
 if __name__ == '__main__':
   info = TVPApi("http://rodzinka.vod.tvp.pl/")
   episodes = info.get_episodes()
-  
+
   for item in episodes:
     episode_ids = item[1]["episode_ids"]
-    
+
     for episode in episode_ids:
       info.download(episode["asset_id"], episode["title"])
